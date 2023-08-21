31717242189a8fb5f5e785546c40fa0ecf9d8df1
Jom Dollesin <codewithjom@gmail.com>
2023-08-21 13:12:55 +0800

    feat: initial commit .

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, incidunt corrupti. Eos ea, ullam nostrum, temporibus beatae veritatis tenetur officiis id vitae optio necessitatibus eum quisquam sequi, quos impedit aliquam!</p>
+  </body>
+</html>
