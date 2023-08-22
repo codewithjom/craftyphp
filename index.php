6ddac4cb6d2aa87a1d3175e7435e0ae4a7be4a29
Jom Dollesin <codewithjom@gmail.com>
2023-08-22 21:51:23 +0800

    feat: added page links.

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php 
 
-  $books = [
-    [
-      'name' => 'Do Androids Dream of Electric Sheep',
-      'author' => 'Philip K. Dick',
-      'releaseYear' => 1968,
-      'purchaseURL' => 'http://example.com'
-    ],
-    [
-      'name' => 'Project Hail Mary',
-      'author' => 'Andy Weir',
-      'releaseYear' => 2021,
-      'purchaseURL' => 'http://example.com'
-    ],
-    [
-      'name' => 'The Martian',
-      'author' => 'Andy Weir',
-      'releaseYear' => 2011,
-      'purchaseURL' => 'http://example.com'
-    ],
-  ];
-
-  $filteredBooks = array_filter($books, function ($book) {
-    return $book['author'] === 'Andy Weir';
-  });
-
-  require 'index.view.php';
+require 'index.view.php';
